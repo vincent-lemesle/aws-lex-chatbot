14073409e7a820949235e1a6f0146efeecb00393
Vincent Lemesle <vincent.lemesle@epitech.eu>
2021-12-25 17:40:42 +0100

    iframe with aws lex

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <iframe src={'https://d19vdtwgy0f60r.cloudfront.net'} width={1200} height={900} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
